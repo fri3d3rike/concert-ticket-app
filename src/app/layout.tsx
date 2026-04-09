5a903889fccc446397aa7f384dae97020fe1e89f
Friederike Koerte <55100138+fri3d3rike@users.noreply.github.com>
2026-04-10 00:16:51 +0200

    fixed icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Grenzen überwinden – Chorkonzert",
   description: "Kirchenchor Konzert Kandern 2026",
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
